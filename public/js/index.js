0d699fe03dbb7ab4aadb98a26494364f0c1ac2b2
TheShonuff <jirvine124@gmail.com>
2022-05-04 17:56:00 -0700

    masonry layout fixed on inital load

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+import Masonry from "masonry-layout";
+
+console.log("Did it work??");
+
+window.onload = () => {
+  const grid = document.querySelector(".grid");
+  console.log("it working");
+  const masonry = new Masonry(grid, {
+    itemSelector: ".grid-item",
+    columnWidth: ".grid-sizer",
+    gutter: 10,
+    percentPosition: true,
+  });
+
+  masonry.on("layoutComplete", () => console.log("Layout Complete"));
+};
